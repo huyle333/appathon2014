2b16657192dded82433c8e0a2e49bc0ff0da31f8
Huy <huy@nat-tvwna-inside-visitornet101-b-29138.princeton.org>
2014-03-28 22:40:11 -0400

    Initial Commit

---
+++ HackPrinceton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HackPrinceton
+//
+//  Created by Huy on 3/28/14.
+//  Copyright (c) 2014 Huy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackPrincetonTests/HackPrincetonTests.m
@@ -0,0 +1,34 @@
+//
+//  HackPrincetonTests.m
+//  HackPrincetonTests
+//
+//  Created by Huy on 3/28/14.
+//  Copyright (c) 2014 Huy Le. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HackPrincetonTests : XCTestCase
+
+@end
+
+@implementation HackPrincetonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
