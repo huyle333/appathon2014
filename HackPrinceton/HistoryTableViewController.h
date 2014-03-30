8cd6ba6a7ac16cd16a9fb3ff513d5d16f9b3c33e
Huy Le <huyle333@yahoo.com>
2014-03-30 07:01:43 -0400

    Added History Table View Controllers

---
+++ HackPrinceton/HistoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewController.h
+//  HackPrinceton
+//
+//  Created by Huy on 3/30/14.
+//  Copyright (c) 2014 Huy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewController : UITableViewController
+
+@end
